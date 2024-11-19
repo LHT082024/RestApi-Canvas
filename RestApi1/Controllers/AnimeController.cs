5966ea5598ffceaa06069ce2508857543d4a714d
LHT082024 <LHT082024@gmail.com>
2024-11-19 11:57:18 +0100

    fixed controller and model

---
+++ RestApi1/Controllers/AnimeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestApi1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AnimeController : Controller
+    {
+        private static List<AnimeController> _gods = new List<AnimeController>()
+        {
+            new AnimeController(
+        }
+
+    }
+}
---
+++ RestApi1/Models/Anime.cs
@@ -0,0 +1,9 @@
+﻿namespace RestApi1.Models
+{
+    public class Anime
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? MyRating { get; set; }
+    }
+}
